0814a615fba1211b9a0c9bbf29bde2bbca67eed9
rkeene <rkeene>
2011-06-13 11:15:37 +0000

    Updated debugging to be off by default, but configurable

--- packages/debug/debug.tcl
+++ packages/debug/debug.tcl
@@ -2,11 +2,22 @@ package provide debug 1.0
 
 namespace eval debug {
 	proc log {src msg} {
+		if {![info exists ::debug::logfile]} {
+		}
+
 		catch {
-			set logfd [open "/var/tmp/webapp-debug.log" a+]
+			set logfd [open $::debug::logfile a+]
 			set usec [lindex [split [expr [format "%u" [clock clicks]].0 / 1000000.0] .] 1]
 			puts $logfd "[clock seconds].$usec \[[pid]\]: \[$src\] => $msg"
 			close $logfd
 		}
+	}
+
+	proc logfile {file} {
+		set ::debug::logfile $file
+	}
+
+	proc logoff {} {
+		unset -nocomplain ::debug::logfile
 	}
 }
