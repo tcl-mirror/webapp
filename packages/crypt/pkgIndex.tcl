0422bccb3a588a5fb5ecc8b8b58b951fd08663e6
rkeene <rkeene>
2011-06-25 22:20:45 +0000

    Fixed typo in last commit

--- packages/crypt/pkgIndex.tcl
+++ packages/crypt/pkgIndex.tcl
@@ -9,4 +9,4 @@
 # full path name of this file's directory.
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded crypt 1.0 [list source [file join $dir crypt.tcl]]
+package ifneeded crypt 1.1 [list source [file join $dir crypt.tcl]]
