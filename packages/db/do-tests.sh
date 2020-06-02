17389b68c3848cf92f7e11f72c0f278e97ca3055
rkeene <rkeene>
2020-06-02 21:27:12 +0000

    Disable tests which require a server

--- packages/db/do-tests.sh
+++ packages/db/do-tests.sh
@@ -5,7 +5,7 @@ time ./test.tcl sqlite || exit 1
 
 echo ''
 echo 'MySQL:'
-time ./test.tcl mysql:webapp_test:unleaded:O09vDXOddHzh3cJiJo49EGzgHkrcNyA3 || exit 1
+#time ./test.tcl mysql:webapp_test:unleaded:O09vDXOddHzh3cJiJo49EGzgHkrcNyA3 || exit 1
 
 echo ''
 echo 'Metakit:'
